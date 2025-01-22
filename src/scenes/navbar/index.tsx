78e489c1c4fba4ac67d0f56de79444527bd6f48d
vule19 <anhvle1901@gmail.com>
2025-01-21 23:32:37 -0500

    enum

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -2,21 +2,24 @@ import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png"
 import Link from "./Link";
+import ActionButton from "@/shared/ActionButton";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
+    isTopOfPage: boolean;
     selectedPage: SelectedPage;
     setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
     const flexBetween = "flex items-center justify-between";
     const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
     const [isMenuToggle, setIsMenuToggle] = useState<boolean>(false);
+    const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
 
   return <nav>
-    <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+    <div className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
             <div className={`${flexBetween} w-full gap-16`}>
                 {/* LEFT SIDE */}
@@ -33,7 +36,7 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                         </div>
                         <div className={`${flexBetween} gap-8`}>
                             <p>Sign In</p>
-                            <button>Become a Member</button>
+                            <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
                         </div>
                     </div>
                 ) : (
@@ -44,6 +47,26 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
             </div>
         </div>
     </div>
+
+    {/* MOBILE MENU MODAL */}
+    {!isAboveMediumScreens && isMenuToggle && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadw-xl">
+            {/* CLOSE ICON */}
+            <div className="flex justify-end p-12">
+                <button onClick={() => setIsMenuToggle(!isMenuToggle)}>
+                    <XMarkIcon className="h-6 w-6 text-gray-400" />
+                </button>
+            </div>
+
+            {/* MENU ITEMS */}
+            <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+                <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            </div>
+        </div>
+    )}
   </nav>;
 };
 
